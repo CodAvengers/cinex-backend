03b1f4c0f1f4dde24120cee6a2a57b9274af6206
Bravin Murambi <bravvjr@gmail.com>
2025-03-31 22:35:12 +0300

    Revert "Merge pull request #14 from CodAvengers/AA-12-list-trending-series"
    This reverts commit 21cbafbabff38892765c661d0eca886f64f753a6, reversing
    changes made to 3d3fbef1bd13e844a7bd0c60d5d21147f45d39ee.

--- routes/authetication.py
+++ routes/authetication.py
@@ -3,7 +3,7 @@ from schemas.schemas import user_schema
 from models import User,db
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
-user_bp = Blueprint('user', __name__, url_prefix='/api')
+user_bp = Blueprint('user', __name__, url_prefix='/api/user')
 
 
 @user_bp.route('/register', methods=['POST'])
@@ -11,7 +11,7 @@ def register():
     data = request.get_json()
 
     # Validate required fields
-    required_fields = ['email', 'password_hash']
+    required_fields = ['email', 'password']
     if not all(field in data for field in required_fields):
         return jsonify({'error': 'Missing required fields'}), 400
 
@@ -23,8 +23,7 @@ def register():
     new_user = User(
         email=data['email']
     )
-
-    new_user.set_password(data['password_hash'])  # Use set_password methodto hash the password
+    new_user.set_password(data['password'])  # Use set_password method to hash the password
 
     # Save user to database
     db.session.add(new_user)
@@ -38,30 +37,3 @@ def register():
         'token': access_token,
         'user': user_schema.dump(new_user)
     }), 201
-
-
-@user_bp.route('/login', methods=['POST'])
-def login():
-    data = request.get_json()
-
-    # Validate input
-    if not data or not data.get('email') or not data.get('password_hash'):
-        return jsonify({'error': 'Email and password required'}), 400
-
-    # Find user by email
-    user = User.query.filter_by(email=data['email']).first()
-    if not user :  # Ensure user exists
-        return jsonify({'error': 'User not found. Kindly register.'}), 401
-
-    if not user.check_password(data['password_hash']):
-        return jsonify({'error': 'Incorrect password.'}), 401
-
-    # Generate JWT token
-    access_token = create_access_token(identity=str(user.id))
-
-    return jsonify({
-        'message': 'Login successful',
-        'access_token': access_token,
-        'user': user_schema.dump(user)
-    }), 200
-
