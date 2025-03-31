18e307ebe4e62d368c55fa51ea3326acc20b1cc1
Eric Richard <reric0802@gmail.com>
2025-03-31 13:36:27 +0300

    AA-12 added genre names

--- routes/authetication.py
+++ routes/authetication.py
@@ -3,7 +3,7 @@ from schemas.schemas import user_schema
 from models import User,db
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
-user_bp = Blueprint('user', __name__, url_prefix='/api/user')
+user_bp = Blueprint('user', __name__, url_prefix='/api')
 
 
 @user_bp.route('/register', methods=['POST'])
@@ -11,7 +11,7 @@ def register():
     data = request.get_json()
 
     # Validate required fields
-    required_fields = ['email', 'password']
+    required_fields = ['email', 'password_hash']
     if not all(field in data for field in required_fields):
         return jsonify({'error': 'Missing required fields'}), 400
 
@@ -23,7 +23,7 @@ def register():
     new_user = User(
         email=data['email']
     )
-    new_user.set_password(data['password'])  # Use set_password methodto hash the password
+    new_user.set_password(data['password_hash'])  # Use set_password method to hash the password
 
     # Save user to database
     db.session.add(new_user)
